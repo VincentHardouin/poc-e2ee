56cf9076130a7c9276549924f4f6aad87243a09d
Vincent Hardouin <vincenthardouin@icloud.com>
2022-01-12 10:31:20 +0100

    improve test

---
+++ README.md
@@ -0,0 +1,7 @@
+# e2ee how does it works ?  
+
+![Diffie-Hellman key exchange](/doc/images/diffie-hellman-key-exchange.png)
+
+![Key management](/doc/images/key-management.png)
+
+![Private key storage](/doc/images/private-key-storage.png)
--- test-e2ee.rb
+++ test-e2ee.rb
@@ -17,15 +17,53 @@ bod_password = 'Pas$W0rd'
 
 # Now you want store Alice private key 
 # Encrypt Alice private key 
-puts alice_keys.private_key
+puts "ALICE Private KEY #{alice_keys.private_key}"
 
-alice_encrypted_priv_key = Aes256GcmEncryption.encrypt(alice_keys.private_key.to_s, alice_password)
+#alice_encrypted_priv_key = Aes256GcmEncryption.encrypt(alice_keys.private_key.to_s, alice_password)
 
-puts "Alice encrypted private key #{alice_encrypted_priv_key}"
+# puts "Alice encrypted private key #{alice_encrypted_priv_key}"
 
 # Decrypt Alice private key 
-alice_dec_priv_key = Aes256GcmEncryption.decrypt(alice_encrypted_priv_key, alice_password)
+#alice_dec_priv_key = Aes256GcmEncryption.decrypt(alice_encrypted_priv_key, alice_password)
 
-puts "Alice decrypt private key #{alice_dec_priv_key}"
-puts "Alice private and decrypt are equal #{alice_keys.private_key.to_s == alice_dec_priv_key}"
 
+alice_pem = alice_keys.to_pem
+puts "alice pem #{alice_pem}"
+
+alice_encrypted_pem = Aes256GcmEncryption.encrypt(alice_pem, alice_password)
+puts "alice pem encrypted #{alice_encrypted_pem}"
+alice_decrypted_pem = Aes256GcmEncryption.decrypt(alice_encrypted_pem, alice_password)
+puts "alice pem decrypted #{alice_decrypted_pem}"
+
+new_instance_alice = OpenSSL::PKey::EC.new(alice_decrypted_pem)
+puts "new instance alice #{new_instance_alice}"
+puts "new instance alice private key #{new_instance_alice.private_key}"
+
+#puts "Alice decrypt private key #{alice_dec_priv_key}"
+#puts "Alice private and decrypt are equal #{alice_keys.private_key.to_s == alice_dec_priv_key}"
+
+
+## Save and Re-use pub key
+puts "Alice pub key #{alice_keys.public_key}"
+
+alice_pub_key_string = alice_keys.public_key.to_bn.to_s
+## Save alice_pub_key_string
+puts "Saved : Alice pub key string #{alice_pub_key_string}"
+
+
+## Re-use alice_pub_key
+
+alice_bn_pub_key = OpenSSL::BN.new(alice_pub_key_string)
+
+new_group = OpenSSL::PKey::EC::Group.new('secp521r1')
+new_alice_pub_key = OpenSSL::PKey::EC::Point.new(new_group, alice_bn_pub_key)
+puts "Alice pub key hash #{new_alice_pub_key}" 
+new_alice_pub_key_string = new_alice_pub_key.to_bn.to_s
+
+puts "Alice pub key string #{new_alice_pub_key_string}"
+p new_alice_pub_key_string == alice_pub_key_string
+
+bob_keys.dh_compute_key(new_alice_pub_key)
+
+
+## Save shared_key 
